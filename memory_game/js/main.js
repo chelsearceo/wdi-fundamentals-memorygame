e8898b2fbd15bee5bc6a66af4ef8fa7ed3f160b7
Chelsea Arceo <chelseaarceo@Chelseas-MBP.fios-router.home>
2019-05-11 16:38:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,30 @@
 console.log("up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+alert('Hello, friends.');
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+cardsInPlay.push("queen");
+cardsInPlay;
+console.log("User flipped queen");
+cardsInPlay.push("king");
+cardsInPlay;
+console.log("User flipped king");
+
+
+var cardOne = cards[1];
+var cardTwo = cards[3];
+
+if (cardsInPlay.length === 2) {	
+}
+else if (cards[1] || cards[2]);
+console.log("User found a match!");
+else if (cards[1] === cards[2]);
+console.log("User did not find a match!");
+
+alert("Sorry, try again.")
+
 
