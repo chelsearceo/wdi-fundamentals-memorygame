86143ddbc0be4406dd50f4fdf1c0172990df12c0
Chelsea Arceo <chelseaarceo@Chelseas-MBP.fios-router.home>
2019-05-12 23:51:14 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,24 +5,48 @@ console.log("User flipped " + cardTwo);
 
 alert('Hello, friends.');
 
-var cards = ["queen", "king", "king", "king"];
-var cardsInPlay = [];
-console.log("User flipped queen");
-console.log("User flipped king");
-
-
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {	
-	if (cardsInPlay[0] === cardsInPlay[1]) { 
-		alert("User found a match!");
-	} else { 
-		alert("User did not find a match!") 
-	}
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king"
+	suit: "hearts"
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king"
+	suit: "diamonds"
+	cardImage: "images/king-of-diamonds.png"
 }
+];
+var cardsInPlay = [];
 
+function checkForMatch() {
+		if (cardsInPlay[0] === cardsInPlay[1]) { 
+			console.log("You found a match");
+		} else { 
+			console.log("Sorry, try again."); 
+		}
+	}
+
+function flipCard(cardId) {
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+};
+flipCard(0);
+flipCard(2);
+checkForMatch();
+	if (cardsInPlay.length === 2) {	
+
+console.log(cardImage);
+console.log(suit);
+		
 
